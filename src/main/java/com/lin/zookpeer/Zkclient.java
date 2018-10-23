e23f3019ce97b8d90f43a80c0b13bff1a8ca745e
liuxing <929593448@qq.com>
2018-10-23 18:34:31 +0800

    ACL自定义权限

--- src/main/java/com/lin/zookpeer/Zkclient.java
+++ src/main/java/com/lin/zookpeer/Zkclient.java
@@ -12,8 +12,9 @@ import org.apache.zookeeper.CreateMode;
  * @date: 2018/7/2 14:48
  */
 public class Zkclient {
+    private static final String zkServerPath = "172.16.1.160:2181";
     public static void main(String[] args) throws Exception{
-        ZkClient zkClient = new ZkClient("192.168.0.183:2181");//建立连接
+        ZkClient zkClient = new ZkClient(zkServerPath);//建立连接
         zkClient.create("/root","mydata", CreateMode.PERSISTENT);//创建目录并写入数据
         String data=zkClient.readData("/root");
         System.out.println(data);
